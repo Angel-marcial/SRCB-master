47ddd29633095250752355a90dd30a74be64d61f
Carlos Quiroz Gonzalez <quirozzap34@gmail.com>
2023-10-29 22:19:21 -0600

    bajas2
    modificacion leve

--- bajas.php
+++ bajas.php
@@ -3,11 +3,12 @@
 include("conexion.php");
 $con = conexion();
 
-$id=$_GET[""];
+$id=$_GET["Ponente."];
 
-$sql="DELETE FROM users WHERE id='$'";
+$sql="DELETE FROM users WHERE id='$'Ponente.";
 $query = mysqli_query($con, $sql);
 
+/*regresa o refresca a ponientes*/
 if($query){
     Header("Location: ponientes.php");
 }else{
